46602d56faa70f54ab54fd81bab3750545f31302
niros1 <niros1@gmail.com>
2022-10-25 11:58:22 +0300

    disable tests

--- src/maxProfit.test.js
+++ src/maxProfit.test.js
@@ -1,6 +1,6 @@
 import { findBestProfitPrices } from "./maxProfit";
 
-if (true) {
+if (false) {
   test("Best Prfit - basic", () => {
     expect(findBestProfitPrices([1, 2, 3, 4, 3, 2, 1, 2, 5])).toEqual([1, 5]);
   });
