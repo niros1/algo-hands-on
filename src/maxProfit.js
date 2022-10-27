97d8fe847f79f071413b41951c3fd0d1b9c86cef
niros1 <niros1@gmail.com>
2022-10-27 09:29:01 +0300

    save 2

--- src/maxProfit.js
+++ src/maxProfit.js
@@ -1,6 +1,5 @@
 export const findBestProfitPrices = function (arrStock) {
   //TODO: Write - Your - Code
-  console.log(arrStock);
 
   return [-1, -1]; //Return a tuple with (high, low) price values
 };
